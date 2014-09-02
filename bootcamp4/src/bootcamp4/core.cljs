be46c0081b2a6fc9885267d95ba37fd6101e7a7a
Shaun Williams <swilliams@pros.com>
2014-09-02 13:38:47 -0500

    baseline for bootcamp4 working

--- bootcamp4/src/bootcamp4/core.cljs
+++ bootcamp4/src/bootcamp4/core.cljs
@@ -1,6 +1,8 @@
 (ns bootcamp4.core
   (:require-macros [hiccups.core :refer [html defhtml]])
   (:require
+    [quiescent]
+    [sablono.core :as sablono :include-macros true]
     [hiccups.runtime]
     [weasel.repl :as repl]))
 
@@ -8,41 +10,25 @@
 
 (enable-console-print!)
 
-(def active-tab (atom "tab4"))
-
-(defn on-change-tab [_keyword _atom old-value new-value]
-  (js/console.log "The tab changed!")
-  (js/console.log (str "The old value was: " old-value))
-  (js/console.log (str "The new value is: " new-value))
-  (.removeClass (js/jQuery ".tab") "active")
-  (.addClass (js/jQuery (str "#" new-value)) "active")
-  )
-
-(add-watch active-tab :change on-change-tab)
-
 (defn by-id
   [id]
   (.getElementById js/document id))
 
-(defn set-html!
-  [id content]
-  (set! (.-innerHTML (by-id id)) content))
+(sablono/defhtml example-app [state]
+  [:div "hi there!"])
 
-(defhtml item
-  [idx]
-  [:li.tab {:id (str "tab" idx)} "Item " idx])
+(def page-state (atom {}))
 
-(set-html! "container"
-           (html [:ul (map item (range 10))]))
-
-(defn on-click-tab [js-event]
-  (let [tab-id (aget js-event "currentTarget" "id")]
-    (reset! active-tab tab-id)))  
-
-(defn init []
-  ;; $("#container").on("click", ".tab", clickTab);
-  (.on (js/jQuery "#container") "click" ".tab" on-click-tab)
-  (swap! active-tab identity)
+(defn on-page-state-change [_ _ old-state new-state]
+  (quiescent/render
+    (example-app new-state)
+    (by-id "appContainer"))
   )
 
-(js/jQuery init)
+(add-watch page-state :change on-page-state-change)
+
+
+(defn init []
+  (swap! page-state identity))
+
+(aset js/window "onload" init)
